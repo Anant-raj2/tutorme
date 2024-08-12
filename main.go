2cecd48c4bf6e4d95826b7a3b44c8d222e172220
Anant Raj <heyanantraj@gmail.com>
2024-08-11 23:23:05 -0500

    worked on initial connection

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Post struct {
+	ID   int    `json:"id"`
+	Body string `json:"body"`
+}
+
+func main() {
+	http.HandleFunc("/hello", HandleRoot)
+	fmt.Println("Running server on http://localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	var post Post = Post{
+		ID:   1,
+		Body: "Hello this is hello world",
+	}
+	postJson, err := json.Marshal(post)
+
+	if err != nil {
+		fmt.Println("Could not unmarshall the post")
+		return
+	}
+	fmt.Println("Sending Post...")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(postJson)
+}
