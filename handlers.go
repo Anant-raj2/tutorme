d1f9aff53bd4a3eee0656ccd8b24af34667ce3b7
Anant Raj <heyanantraj@gmail.com>
2024-08-13 02:38:27 -0500

    worked on more handlers'

--- handlers.go
+++ handlers.go
@@ -64,4 +64,77 @@ func (h *Handlers) Health(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// HelloWorld is a helloworld HTTP handler
+func (h *Handlers) HelloWorld(w http.ResponseWriter, r *http.Request) error {
+	contentType := r.Header.Get("Content-Type")
+	switch contentType {
+	case "application/json":
+		{
+			webgo.SendResponse(w, "hello world", http.StatusOK)
+		}
+	default:
+		{
+			buff := bytes.NewBufferString("")
+			err := h.home.Execute(
+				buff,
+				struct {
+					Message string
+				}{
+					Message: "Welcome to the Home Page!",
+				},
+			)
+			if err != nil {
+				return errors.InternalErr(err, "Inter server error")
+			}
 
+			w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+			_, err = w.Write(buff.Bytes())
+			if err != nil {
+				return errors.Wrap(err, "failed to respond")
+			}
+		}
+	}
+	return nil
+}
+
+func errWrapper(h func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := h(w, r)
+		if err == nil {
+			return
+		}
+
+		status, msg, _ := errors.HTTPStatusCodeMessage(err)
+		webgo.SendError(w, msg, status)
+		if status > 499 {
+			logger.Error(r.Context(), errors.Stacktrace(err))
+		}
+	}
+}
+
+func panicRecoverer(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	defer func() {
+		p := recover()
+		if p == nil {
+			return
+		}
+		webgo.R500(w, errors.DefaultMessage)
+
+		logger.Error(r.Context(), fmt.Sprintf("%+v", p))
+		fmt.Println(string(debug.Stack()))
+	}()
+
+	next(w, r)
+}
+
+func loadHomeTemplate(basePath string) (*template.Template, error) {
+	t := template.New("index.html")
+	home, err := t.ParseFiles(
+		fmt.Sprintf("%s/index.html", basePath),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed parsing templates")
+	}
+
+	return home, nil
+}
