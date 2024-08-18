6f8fcec735c830eeb454fd4d6314b8bacb455458
Anant Raj <heyanantraj@gmail.com>
2024-08-18 04:44:54 -0500

    worked on debugging and figuring out a way to get the tailwind clie

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,12 +2,12 @@ package auth
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Anant-raj2/tutorme/internal/db"
-	"github.com/Anant-raj2/tutorme/pkg/util"
-	"github.com/Anant-raj2/tutorme/web/templa"
+	"github.com/Anant-raj2/tutorme/web/templa/auth"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -22,27 +22,31 @@ func New(queries *db.Queries) *AuthStore {
 }
 
 func (handler *AuthStore) RenderSignup(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-	component := templa.Hello("Anant-raj2")
+	component := auth.SignupForm("Anant-raj2")
 	component.Render(context.Background(), w)
 }
 
 func (handler *AuthStore) CreateTutor(w http.ResponseWriter, r *http.Request, _ httprouter.Params) error {
 	ctx := context.Background()
 	r.ParseForm()
-	var user util.TutorParams = util.TutorParams{
+	grade_level, err := strconv.Atoi(r.FormValue("grade_level"))
+
+	var tutorConfig db.CreateTutorParams = db.CreateTutorParams{
 		Email:      r.FormValue("email"),
 		Name:       r.FormValue("name"),
 		Gender:     r.FormValue("gender"),
-		GradeLevel: 11,
+		GradeLevel: int32(grade_level),
 		Role:       r.FormValue("role"),
 		Subject:    r.FormValue("subject"),
 	}
 
-	tutor, err := handler.queries.CreateTutor(ctx, *user.OTD())
+	tutor, err := handler.queries.CreateTutor(ctx, tutorConfig)
 	if err != nil {
 		return err
 	}
 
-	json.NewEncoder(w).Encode(tutor)
+	fmt.Println(tutor)
+	component := auth.SignupForm("Anant-raj2")
+	component.Render(context.Background(), w)
 	return nil
 }
