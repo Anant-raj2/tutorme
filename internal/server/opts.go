ac48668003dcb195fd3b71a049ab44d0c837f086
Anant Raj <heyanantraj@gmail.com>
2024-08-20 17:43:29 -0500

    added the panic recoverer

--- internal/server/opts.go
+++ internal/server/opts.go
@@ -10,3 +10,24 @@ type Config struct {
 	ReadTimeout  time.Duration
 	WriteTimeout time.Duration
 }
+
+func panicRecoverer(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	defer func() {
+		p := recover()
+		if p == nil {
+			return
+		}
+		webgo.R500(w, errors.DefaultMessage)
+
+		logger.Error(r.Context(), fmt.Sprintf("%+v", p))
+		fmt.Println(string(debug.Stack()))
+	}()
+
+	next(w, r)
+}
+
+// Handlers struct has all the dependencies required for HTTP handlers
+type Handlers struct {
+	apis api.Server
+	home *template.Template
+}
