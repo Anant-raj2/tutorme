51bcdc88fbaf4c1d47409eec0abe36245c2266f9
Anant Raj <heyanantraj@gmail.com>
2024-08-28 03:24:37 -0500

    continue working on matching

--- pkg/util/matching.go
+++ pkg/util/matching.go
@@ -90,3 +90,75 @@ func fetchTutors(ctx context.Context) ([]Tutor, error) {
 
 	return tutors, nil
 }
+
+func fetchStudents(ctx context.Context) ([]Student, error) {
+	rows, err := pool.Query(ctx, "SELECT id, name, subjects_needed, grade FROM students")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var students []Student
+	for rows.Next() {
+		var s Student
+		err := rows.Scan(&s.ID, &s.Name, &s.SubjectsNeeded, &s.Grade)
+		if err != nil {
+			return nil, err
+		}
+		students = append(students, s)
+	}
+
+	return students, nil
+}
+
+func performMatching(tutors []Tutor, students []Student) []Match {
+	var matches []Match
+
+	for _, student := range students {
+		for _, tutor := range tutors {
+			score := calculateMatchScore(tutor, student)
+			if score > 0 {
+				matches = append(matches, Match{
+					TutorID:   tutor.ID,
+					StudentID: student.ID,
+					Score:     score,
+				})
+			}
+		}
+	}
+
+	sort.Slice(matches, func(i, j int) bool {
+		return matches[i].Score > matches[j].Score
+	})
+
+	return matches[:min(len(matches), 10)] // Return top 10 matches
+}
+
+func calculateMatchScore(tutor Tutor, student Student) float64 {
+	var score float64
+
+	for _, subject := range student.SubjectsNeeded {
+		for _, tutorSubject := range tutor.Subjects {
+			if subject == tutorSubject {
+				score += 1.0
+				break
+			}
+		}
+	}
+
+	if score == 0 {
+		return 0
+	}
+
+	experienceBonus := float64(tutor.Experience) * 0.1
+	score += experienceBonus
+
+	return score
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
